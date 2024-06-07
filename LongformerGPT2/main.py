2d1442bb7bcbbdc19947544f93fd2bf70a44cdff
MeteorVanish <Zhenyu_Wei@sjtu.edu.cn>
2024-06-07 14:34:20 +0800

    longformer test

--- LongformerGPT2/main.py
+++ LongformerGPT2/main.py
@@ -1,7 +1,7 @@
 import torch
 from thop import profile
 from transformers import GPT2Tokenizer
-from LongformerGPT2 import LongformerGPT2LMHeadModel, LongformerGPT2Config
+from LongformerGPT2 import LongformerGPT2LMHeadModel, LongformerGPT2Config, LongformerSelfAttention, SelfAttention
 from sliding_chunks import pad_to_window_size
 
 config = LongformerGPT2Config.from_pretrained('./longformer_gpt2_model/')
@@ -21,7 +21,7 @@ tokenizer = GPT2Tokenizer.from_pretrained('./gpt2_model/')
 tokenizer.model_max_length = 4096
 
 # GPT2 pretrained seq max length = 1024, thus too long input is invalid
-SAMPLE_TEXT = ' '.join(['Hello world! '] * 8)  # long input document
+SAMPLE_TEXT = ' '.join(['Hello world! '] * 64)  # long input document
 
 input_ids = torch.tensor(tokenizer.encode(SAMPLE_TEXT)).unsqueeze(0)  # batch of size 1
 
@@ -42,6 +42,30 @@ if input_ids.max() >= tokenizer.vocab_size:
 outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
 print(outputs[0].item())
 
-flops, params = profile(model, inputs=(input_ids,))
-print(f'FLOPs: {flops / 1024 ** 3} B')
-print(f'Parameters (from profile): {params / 1024 ** 2} M')
+
+import time
+SEQ_LEN = [16, 32, 64, 128, 256, 512, 1024]
+
+print("Longformer")
+for seq_len in SEQ_LEN:
+    SAMPLE_TEXT = ' '.join(['Hello world! '] * seq_len)
+    input_ids = torch.tensor(tokenizer.encode(SAMPLE_TEXT)).unsqueeze(0)
+    attention = LongformerSelfAttention(config, 0)
+    inputs = model.transformer.wte(input_ids)
+    inputs = torch.cat([inputs, inputs], dim=0)
+    start = time.time()
+    attention(inputs)
+    end = time.time()
+    print(f'SeqLen = {seq_len * 4}, Time: {end - start} s')
+
+print("GPT-2")
+for seq_len in SEQ_LEN:
+    SAMPLE_TEXT = ' '.join(['Hello world! '] * seq_len)
+    input_ids = torch.tensor(tokenizer.encode(SAMPLE_TEXT)).unsqueeze(0)
+    attention = SelfAttention(n_head=12, d_k=768, d_v=768, d_x=768, d_o=768)
+    inputs = model.transformer.wte(input_ids)
+    inputs = torch.cat([inputs, inputs], dim=0)
+    start = time.time()
+    attention(inputs)
+    end = time.time()
+    print(f'SeqLen = {seq_len * 4}, Time: {end - start} s')
